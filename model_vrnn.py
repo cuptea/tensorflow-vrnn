92a17316a8047eafad2eb082edf2b165d4ee3f53
Anirudh Vemula <vvanirudh@gmail.com>
2016-12-04 22:28:04 -0500

    VRNN init documented

--- model_vrnn.py
+++ model_vrnn.py
@@ -179,105 +179,195 @@ class VartiationalRNNCell(tf.nn.rnn_cell.RNNCell):
         return (enc_mu, enc_sigma, dec_mu, dec_sigma, dec_rho, prior_mu, prior_sigma), state2
 
 
-
-
 class VRNN():
+    '''
+    Class for the VRNN network
+    '''
     def __init__(self, args, sample=False):
+        '''
+        Initialization function
+        Params:
+        args : Arguments for the model
+        sample : Training/inference mode
+        '''
+        # Helper functions
 
         def tf_normal(y, mu, s, rho):
+            '''
+            Computes the log likelihood w.r.t Gaussian distribution
+            Params:
+            y : input data
+            mu : mean
+            s : sigma
+            rho : correlation
+            '''
+            # Define variable scope
             with tf.variable_scope('normal'):
-                ss = tf.maximum(1e-10,tf.square(s))
-                norm = tf.sub(y[:,:args.chunk_samples], mu)
+                # Compute sigma squared (or variance)
+                ss = tf.maximum(1e-10, tf.square(s))
+                # Compute (y - mu)
+                norm = tf.sub(y[:, :args.chunk_samples], mu)
+                # Compute (y-mu)^2/sigma^2
                 z = tf.div(tf.square(norm), ss)
+                # Compute denominator log(2*pi*sigma^2)
                 denom_log = tf.log(2*np.pi*ss, name='denom_log')
-                result = tf.reduce_sum(z+denom_log, 1)/2# -
-                                       #(tf.log(tf.maximum(1e-20,rho),name='log_rho')*(1+y[:,args.chunk_samples:])
-                                       # +tf.log(tf.maximum(1e-20,1-rho),name='log_rho_inv')*(1-y[:,args.chunk_samples:]))/2, 1)
+
+                # TODO Figure out what this exactly does
+                result = tf.reduce_sum(z+denom_log, 1)/2
+                # -
+                # (tf.log(tf.maximum(1e-20,rho),name='log_rho')*(1+y[:,args.chunk_samples:])
+                # +tf.log(tf.maximum(1e-20,1-rho),name='log_rho_inv')*(1-y[:,args.chunk_samples:]))/2, 1)
 
             return result
 
         def tf_kl_gaussgauss(mu_1, sigma_1, mu_2, sigma_2):
+            '''
+            Function to compute the KL-divergence between two Gaussian
+            distributions
+            Params:
+            mu_1, sigma_1 : mean and std dev of first distribution
+            mu_2, sigma_2 : mean and std dev of second distribution
+            '''
+            # Define variable scope
             with tf.variable_scope("kl_gaussgauss"):
+                # Compute the KL-divergence term given in
+                # Auto-encoding VB paper eqn. 10
+                # TODO derive this
                 return tf.reduce_sum(0.5 * (
-                    2 * tf.log(tf.maximum(1e-9,sigma_2),name='log_sigma_2') 
-                  - 2 * tf.log(tf.maximum(1e-9,sigma_1),name='log_sigma_1')
-                  + (tf.square(sigma_1) + tf.square(mu_1 - mu_2)) / tf.maximum(1e-9,(tf.square(sigma_2))) - 1
+                    2 * tf.log(tf.maximum(1e-9, sigma_2), name='log_sigma_2')
+                    - 2 * tf.log(tf.maximum(1e-9, sigma_1), name='log_sigma_1')
+                    + (tf.square(sigma_1) + tf.square(mu_1 - mu_2)) / tf.maximum(1e-9, (tf.square(sigma_2))) - 1
                 ), 1)
 
         def get_lossfunc(enc_mu, enc_sigma, dec_mu, dec_sigma, dec_rho, prior_mu, prior_sigma, y):
+            '''
+            Function to compute loss given the predicted parameters and the true data
+            Params:
+            enc_mu, enc_sigma : mean and stddev for encoder
+            dec_mu, dec_sigma : mean and stddev for decoder
+            dec_rho : correlation for decoder
+            prior_mu, prior_sigma : mean and stddev for prior distribution
+            y : true data
+            '''
+            # Compute KL divergence between encoder distribution and the prior distribution
             kl_loss = tf_kl_gaussgauss(enc_mu, enc_sigma, prior_mu, prior_sigma)
+            # Compute the log likelihood of the true data w.r.t the decoder distribution (reconstruction error)
             likelihood_loss = tf_normal(y, dec_mu, dec_sigma, dec_rho)
 
+            # Add both the losses to get the final loss
             return tf.reduce_mean(kl_loss + likelihood_loss)
-            #return tf.reduce_mean(likelihood_loss)
+            # return tf.reduce_mean(likelihood_loss)
 
+        # Store input arguments
         self.args = args
+
+        # If in inference mode, then batch size and sequence length is 1
         if sample:
             args.batch_size = 1
             args.seq_length = 1
 
+        # Define the VRNNCell
+        # TODO what is args.chunk_samples?
         cell = VartiationalRNNCell(args.chunk_samples, args.rnn_size, args.latent_size)
 
+        # Store the cell
         self.cell = cell
 
+        # Define placeholders for the input, target data
+        # input_data would be of size batch_size x seq_length x (2*chunk_samples)
         self.input_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2*args.chunk_samples], name='input_data')
-        self.target_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2*args.chunk_samples],name = 'target_data')
-        self.initial_state_c, self.initial_state_h = cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)
 
+        # target_data would be of size batch_size x seq_length x (2*chunk_samples)
+        self.target_data = tf.placeholder(dtype=tf.float32, shape=[args.batch_size, args.seq_length, 2*args.chunk_samples], name='target_data')
+
+        # Initialize the cell state and the hidden state of the VRNNCell (remember state_is_tuple is true)
+        self.initial_state_c, self.initial_state_h = cell.zero_state(batch_size=args.batch_size, dtype=tf.float32)
 
         # input shape: (batch_size, n_steps, n_input)
         with tf.variable_scope("inputs"):
-            inputs = tf.transpose(self.input_data, [1, 0, 2])  # permute n_steps and batch_size
-            inputs = tf.reshape(inputs, [-1, 2*args.chunk_samples]) # (n_steps*batch_size, n_input)
+            # Permute the input data to be of shape (seq_length x batch_size x n_input)
+            inputs = tf.transpose(self.input_data, [1, 0, 2])
+            # Reshape so that the shape is ((seq_length*batch_size) x n_input)
+            inputs = tf.reshape(inputs, [-1, 2*args.chunk_samples])
 
             # Split data because rnn cell needs a list of inputs for the RNN inner loop
-            inputs = tf.split(0, args.seq_length, inputs) # n_steps * (batch_size, n_hidden)
-        flat_target_data = tf.reshape(self.target_data,[-1, 2*args.chunk_samples])
+            inputs = tf.split(0, args.seq_length, inputs)  # n_steps * (batch_size, n_input)
 
+        # Flatten the target data to be of shape ((batch_size*seq_length) x n_input)
+        flat_target_data = tf.reshape(self.target_data, [-1, 2*args.chunk_samples])
+
+        # Store flattened target data
         self.target = flat_target_data
-        self.flat_input = tf.reshape(tf.transpose(tf.pack(inputs),[1,0,2]),[args.batch_size*args.seq_length, -1])
+
+        # Flatten input data in the same way as target_data was flattened
+        self.flat_input = tf.reshape(tf.transpose(tf.pack(inputs), [1, 0, 2]), [args.batch_size*args.seq_length, -1])
+
+        # Store the list of inputs
         self.input = tf.pack(inputs)
+
         # Get vrnn cell output
-        outputs, last_state = tf.nn.rnn(cell, inputs, initial_state=(self.initial_state_c,self.initial_state_h))
-        #print outputs
-        #outputs = map(tf.pack,zip(*outputs))
+        outputs, last_state = tf.nn.rnn(cell, inputs, initial_state=(self.initial_state_c, self.initial_state_h))
+
+        # print outputs
+        # outputs = map(tf.pack,zip(*outputs))
+
         outputs_reshape = []
         names = ["enc_mu", "enc_sigma", "dec_mu", "dec_sigma", "dec_rho", "prior_mu", "prior_sigma"]
-        for n,name in enumerate(names):
+        for n, name in enumerate(names):
             with tf.variable_scope(name):
+                # Pack the list of values into a tensor
                 x = tf.pack([o[n] for o in outputs])
-                x = tf.transpose(x,[1,0,2])
-                x = tf.reshape(x,[args.batch_size*args.seq_length, -1])
+                # Permute so that it is of shape batch_size x seq_length x n_input
+                x = tf.transpose(x, [1, 0, 2])
+                # Convert it into the shape of flattened data
+                x = tf.reshape(x, [args.batch_size*args.seq_length, -1])
+                # Append to list of outputs
                 outputs_reshape.append(x)
 
+        # Extract tensors of predicted parameters
         enc_mu, enc_sigma, dec_mu, dec_sigma, dec_rho, prior_mu, prior_sigma = outputs_reshape
-        self.final_state_c,self.final_state_h = last_state
+
+        # Store the final cell and hidden state of the VRNNCell
+        self.final_state_c, self.final_state_h = last_state
+
+        # Store the decoder parameters
         self.mu = dec_mu
         self.sigma = dec_sigma
         self.rho = dec_rho
 
+        # Compute the loss
         lossfunc = get_lossfunc(enc_mu, enc_sigma, dec_mu, dec_sigma, dec_sigma, prior_mu, prior_sigma, flat_target_data)
-        self.sigma = dec_sigma
-        self.mu = dec_mu
+
+        # Store the loss as cost
         with tf.variable_scope('cost'):
-            self.cost = lossfunc 
+            self.cost = lossfunc
+
         tf.scalar_summary('cost', self.cost)
         tf.scalar_summary('mu', tf.reduce_mean(self.mu))
         tf.scalar_summary('sigma', tf.reduce_mean(self.sigma))
 
-
+        # Learning rate
         self.lr = tf.Variable(0.0, trainable=False)
+        # Get all trainable variables
         tvars = tf.trainable_variables()
+
+        # Print the names of all trainable variables
         for t in tvars:
             print t.name
+
+        # Compute gradients for all trainable variables
         grads = tf.gradients(self.cost, tvars)
-        #grads = tf.cond(
+        # grads = tf.cond(
         #    tf.global_norm(grads) > 1e-20,
         #    lambda: tf.clip_by_global_norm(grads, args.grad_clip)[0],
         #    lambda: grads)
+
+        # Define the optimizer
         optimizer = tf.train.AdamOptimizer(self.lr)
+
+        # Define the train operator and apply gradients
         self.train_op = optimizer.apply_gradients(zip(grads, tvars))
-        #self.saver = tf.train.Saver(tf.all_variables())
+        # self.saver = tf.train.Saver(tf.all_variables())
 
     def sample(self, sess, args, num=4410, start=None):
 
